4c7745e80e6c2d7bc2b908cda801feb78bcd7d88
Nikita Stepanovs(nixory) <nikitastepanovs777@gmail.com>
2022-10-06 13:24:49 +0300

    task 1

--- LAB1/main.cpp
+++ LAB1/main.cpp
@@ -2,11 +2,22 @@
 
 int main() 
 { 
-	int a; 
-	int res; 
-	printf("Enter number: "); 
-	scanf_s("%i", &a); 
-	res = a * a; 
+	int a, b; 
+	int res, sum, diff, mult;
+	float div;
+	printf("Enter number 1: "); 
+	scanf_s("%i", &a);
+	printf("Enter number 2: ");
+	scanf_s("%i", &b);
+	res = a * a;
+	sum = a + b;
+	diff = a - b;
+	mult = a * b;
+	div = float(a) / b;
 	printf("Square of %i is %i\n", a, res); 
+	printf("%i + %i = %i\n", a, b, sum);
+	printf("%i - %i = %i\n", a, b, diff);
+	printf("%i * %i = %i\n", a, b, mult);
+	printf("%i / %i = %.2f\n", a, b, div);
 	return 0; 
 } 
